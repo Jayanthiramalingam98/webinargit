c5b476d936554c8b95762eb042f0b6b771e6ce19
Jayanthi Ramalingam <jayanthir@stg.com>
2022-06-21 18:35:46 +0530

    message

---
+++ src/test/java/GKEdeploy/GKE/GkeApplicationTests.java
@@ -0,0 +1,13 @@
+package GKEdeploy.GKE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
